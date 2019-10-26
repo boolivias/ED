278f0885d5cf71ac3f8aa52624dc3cdffbf26990
boolivias <jean_wylmer@hotmail.com>
2019-10-26 13:46:45 -0300

    LAB 09 COMPLETO, CASOS-TESTE 100%

---
+++ lab09/fila.c
@@ -0,0 +1,85 @@
+#include "fila.h"
+
+void new_Fila(Fila *p, int Size);
+int isEmpty(Fila *p);
+int getFull(Fila *p);
+int Add(Fila *p, T item, int op);
+int Remove(Fila *p, T *item, int op);
+void Clear(Fila *p);
+void Print(Fila *p);
+int getSize(Fila*p);
+int Count(Fila *p);
+
+void new_Fila(Fila *p, int Size){
+    p->Start = 0;
+    p->End = 0;
+    p->Size = Size+1; //o tamanho adicional é o espaço de sacrificio
+                      //para verificar se a fila está cheia ou vazia
+}
+
+int isEmpty(Fila *p){
+    if( p->Start == p->End )
+        return 1; //Retorna 1 se a fila está vazia
+    return 0;     //0 Se não está vazia
+}
+
+int getFull(Fila *p){
+    if( p->Start==0 && (p->End == getSize(p)-1) )
+        return 1;
+    if( p->End+1 == p->Start )
+        return 1;
+
+    return 0; //Retorna 0 somente no caso da fila não estiver cheia
+}
+
+int Add(Fila *p, T item, int op){
+    if( getFull(p) == 0 ){
+        if( op == 0 ){
+            p->Item[p->End++] = item;
+            if( p->End == getSize(p) )
+                p->End = 0;
+        }
+        else
+            if( p->Start > 0 || p->Start > p->End )
+                p->Item[--p->Start] = item;
+            else{
+                p->Start = getSize(p)-1;
+                p->Item[p->Start] = item;
+            }
+        return 0;
+    }
+    return 1;
+}
+
+int Remove(Fila *p, T *item, int op){
+    if( isEmpty(p) == 0 ){
+        if( op == 0 )
+            if( p->End > 0 )
+                *item = p->Item[--p->End];
+            else{
+                p->End = getSize(p)-1;
+                *item = p->Item[p->End];
+            }
+        else{
+            *item = p->Item[p->Start++];
+            p->Start = p->Start == getSize(p) ? 0 : p->Start;
+        }
+        return 0;
+    }
+    return 1;
+}
+void Clear(Fila *p){
+    new_Fila(p, p->Size);
+}
+void Print(Fila *p){
+    for(; !isEmpty(p); p->Start++){
+        p->Start = p->Start < getSize(p) ? p->Start : 0;
+        printf("%d ",p->Item[p->Start]);
+    }
+}
+int getSize(Fila*p){
+    return p->Size;
+}
+int Count(Fila *p){
+    return p->Start < p->End ? p->End - p->Start : (getSize(p) - p->Start) + p->End+2;
+}
---
+++ lab09/fila.h
@@ -0,0 +1,44 @@
+#ifndef FILA_H
+#define FILA_H
+
+#define MAX 550001
+
+typedef int T;
+
+typedef int Index;
+
+typedef struct fila{
+    T Item[MAX];
+    Index Start;
+    Index End;
+    int Size;
+} Fila;
+
+//Função que instancia a fila
+void new_Fila(Fila *p, int Size);
+
+//Função que verifica se a fila está vazia
+int isEmpty(Fila *p);
+
+//Função que verifica se a fila está cheia
+int getFull(Fila *p);
+
+//Função que insere o item na fila, no ínicio ou no final(definido por op)
+int Add(Fila *p, T item, int op);
+
+//Função que remove o item da fila no ínicio ou no final(definido por op), e coloca no ponteiro item
+int Remove(Fila *p, T *item, int op);
+
+//Função que esvazia a fila
+void Clear(Fila *p);
+
+//Função que printa a fila
+void Print(Fila *p);
+
+//Função que retorna o tamanho máximo da fila
+int getSize(Fila*p);
+
+//Função que retorna o número de elementos na fila
+int Count(Fila *p);
+
+#endif // FILA_H
